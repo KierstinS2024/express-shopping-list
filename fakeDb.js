aaf837c658fc3952589d3320b30321533b723922
KierstinS2024 <180041125+KierstinS2024@users.noreply.github.com>
2025-08-11 14:52:39 -0600

    Add items to fakeDb.js remove global

--- fakeDb.js
+++ fakeDb.js
@@ -1,4 +1,16 @@
 // fakeDb.js
-global.items = [];
+
+const items = [
+  { name: "popsicle", price: 1.45 },
+  { name: "cheerios", price: 3.4 },
+  { name: "milk", price: 2.99 },
+  { name: "eggs", price: 2.5 },
+  { name: "bread", price: 2.75 },
+  { name: "bananas", price: 1.2 },
+  { name: "coffee", price: 4.99 },
+  { name: "pizza", price: 6.5 },
+  { name: "ice cream", price: 3.99 },
+  { name: "toothpaste", price: 2.1 },
+];
 
 module.exports = items;
